922f13717e2eee2c84e67789e8a5de6d44d5056b
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-05-17 20:08:04 -0700

    Changes made working on for loop

--- script.js
+++ script.js
@@ -1,71 +1,76 @@
 // Assignment Code
 var lowcase = "abcdefghijklmnopqrstuvwxyz";
 var upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "1234567890";
+var numbers = "1234567890";
 var special = "!#-$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+var lowcaseOpt = false;
+var upcaseOpt = false;
+var numbersOpt = false;
+var specOpt = false;
+
+var passwordlength;
+var userChoice = "";
+var password
 
 //all these functions need to be in one//
 function passwordOptions() {
-  var passwordlength = parseInt(prompt("Choose desired password length between 8 and 128"));  // 10
+  var userChoice = "";
+  var lowcaseOpt = false;
+  var upcaseOpt = false;
+  var numbersOpt = false;
+  var specOpt = false;
+
+  var passwordlength = parseInt(prompt("Choose desired password length between 8 and 128"));
 
   if (isNaN(passwordlength) === true) {
     alert("password length must be a number")
     return;
   }
-
   if (passwordlength < 8 || passwordlength > 128) {
     alert("Must choose length between 8 and 128")
     return;
   }
-
-
+//do i need to store these answers and also do i need to put these with option variable line 59
   var lowcaseOpt = window.confirm("Include lowcase letters?");
-  //have to be able to store response
+  if (lowcaseOpt === true) {
+    userChoice = userChoice + lowcase;    
+  } 
 
   var upcaseOpt = window.confirm("Include UPPERCASE letter?");
-  //have to store response here
+  if (upcaseOpt === true) {
+    userChoice = userChoice + upcase;
+  }
 
-  var numOpt = window.confirm("Include numbers?");
-  //have to store response
+  var numbersOpt = window.confirm("Include numbers?");
+  if (numbersOpt === true) {
+    userChoice = userChoice + numbers;
+  }
 
   var specOpt = window.confirm("Include special characters?");
-  //have to store response
-
-  //if they do not at least say "OK" to one of the prompts needs to be a window reminding them that they must choose one//
-  //need this to be === false so that if all of them are false it will show this alert//
-  window.alert("Must choose at least one special charachter option");
-  //return so that user can start process over again//
-
-
-  var options = {
-    passwordlength: passwordlength,
-    lowcaseOpt: lowcaseOpt,
-    upcaseOpt: upcaseOpt,
-    numOpt:numOpt,
-    specOpt:specOpt
+    if (specOpt === true) {
+      userChoice = userChoice + special;
   }
+  if (lowcaseOpt === false, numOpt === false, upcaseOpt === false, specOpt === false) {
+    alert("Must choose one character option");
+    generatePassword();
+  }
+};
+var password = ""
 
-  return options;
+// loop as many time as your pass length is based on index of your string and return character,false statemants not needed becasue we only care if its true//
+//make sure its length and not password length//
+for (var i = 0; i< length; i++) {
+  var random = Math.floor(Math.random() * userChoice.length);
+
+  password = password + userChoice.charAt(random);
 }
 
+  return password;
 
-function generatePassword() {
-  var getOptions = passwordOptions()
-  var garanteedChar;
-  var results = [];
-
-  if (getOptions.lowcaseOpt === true) {
-    garanteedChar = garanteedChar.concat(lowcase)
-  }
-  if (getOptions.upcaseOpt === true) {
-    garanteedChar = garanteedChar.concat(upcase)
-    
-  }
-  // loop as many time as your pass length is based on index of your string and return character
-}
+};
 
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
@@ -74,7 +79,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
